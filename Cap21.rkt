53968c701f001b9a3a1b796de03cba9285b57a86
= <=>
2022-09-30 18:17:40 +0200

    Ejercicios 3 y 4 hechos, falta el 2

--- Cap21.rkt
+++ Cap21.rkt
@@ -1,16 +1,36 @@
 #lang racket
-(define (TRUE x y) x)
-(define (FALSE x y) y)
-
-(define (AND x y)
-  (if (= x 1)
-      (if (= y 1)
-          (TRUE x y)
-           0)
-    0))
-
-;(define (cons x y) (lambda (m) (m x y)));
+;EJERCICIO 1: Compruebe con el modelo de substitución que (car (cons x y)) es x, además programe la función cdr correspondiente.
 (define (H p q) p)
 (define (J p q) q)
 (define (car z0) (z0 H))
 (define (cdr z1) (z1 J))
+;Probamos el modelo de substitución para el caso x=1 y=2
+;car(cons(1 2)) -> car(lambda z(z 1 2)) -> (lambda z(z 1 2)H) -> (H 1 2) -> 1.
+
+;EJERCICIO 3: Defina 1 y 2 como funciones y defina la suma directamente como la composición de los dos elementos.
+(define zero (lambda (f) (lambda (x) x)))
+
+(define (add-1 n)
+  (lambda (f) (lambda (x) (f ((n f) x)))))
+
+(define (add-2 n)
+  (lambda (f) (lambda (f) (lambda (x) (f ((n f) x))))))
+
+(define (suma n)
+  (lambda f (lambda (f) (lambda (f) (lambda (x) (f ((n f) x)))))))
+
+;EJERCICIO 4: Defina los operadores lógicos usando solamente estas dos funciones.
+(define (TRUE x y) x)
+(define (FALSE x y) y)
+;Funcion NOT
+(define (NOT BOOL)
+  (lambda (x y) (BOOL y x))
+ )
+;Funcion OR
+(define (OR BOOL1 BOOL2)
+  (lambda (x y) (BOOL1 (BOOL1 x y) (BOOL2 x y)))
+)
+;Funcion AND
+(define (AND BOOL1 BOOL2)
+  (NOT (OR (NOT BOOL1) (NOT BOOL2)))
+ )
