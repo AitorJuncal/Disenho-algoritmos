caf68f523fd4cd99765f34f5afd5abf5061f18b2
= <=>
2022-10-21 15:58:44 +0200

    ej 1 del cap22

--- Cap22.rkt
+++ Cap22.rkt
@@ -1,11 +1,8 @@
 #lang racket
-(define (combinatorio n k)
-  (cond ((> k n) 0)
-        ((= n 0) 1)
-        ((= k 0) 1)
-        (else (+ (combinatorio (- n 1) (- k 1)) (combinatorio (- n 1) k)))))
+(define (new-row-temp anterior)
+  (if (null? (cdr anterior))
+      (list (car anterior))
+      (cons (+ (car anterior) (car (cdr anterior))) (new-row-temp (cd anterior)))))
 
-
-(define lista (list 1 2 3 4))
-(define (square x) (* x x))
-(map square lista)
+(define (new-row anterior)
+  (cons 1 (new-row-temp anterior)))
