93c2adaf7afa08df96363f7a131b707aff2056f8
= <=>
2022-11-22 16:32:14 +0100

    cap3 terminado

--- Cap3.rkt
+++ Cap3.rkt
@@ -24,10 +24,11 @@
 (s 100)
 (s 'how-many-calls?)
 
-
 ;Ejercicio 3
-(define f   
-   (let ((init 0)) 
+(define f  
+    (let ((init 0))  
      (lambda (x)  
-       (set! init (- x init))  
-       (- x init))))
+         (set! init  (- x init)) 
+         init)))
+(+ (f 0) (f 1))
+(+ (f 1) (f 0))
